3b2866526b852cb16b2be63b6a250a1df28c591c
Alaa Khoulani <102520129+alaaKhoulani@users.noreply.github.com>
2024-06-03 21:51:06 +0300

    testing

---
+++ tt.py
@@ -0,0 +1,8 @@
+from matching_and_ranking import set_cprpus_global_variable
+from word_embedding import set_global_variable,ranking
+
+set_cprpus_global_variable()
+set_global_variable()
+
+print(ranking("science","hi"))
+
